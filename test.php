2d9f9a9b9222b11b5db62e69e6dbee19c2633fe9
Rakesh Katti <rakesh.katti@gmail.com>
2014-03-04 15:05:24 +0530

    Added form

---
+++ test.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Testing Form</title>
+</head>
+<body>
+
+<form action="" method="post">
+Name <input type="text" name="name" />
+E Mail<input type="text" name="email" />
+<input type="submit" value="button" />
+</form>
+	
+</body>
+</html>
